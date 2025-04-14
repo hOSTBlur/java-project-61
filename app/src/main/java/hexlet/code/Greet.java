86dc085f3ae9c3265f9361fc8f76cf990711bea2
Grouler <vad.abdurahmanov@gmail.com>
2025-04-15 02:11:29 +0300

    Change filename from Cli to Greet.java

--- app/src/main/java/hexlet/code/Greet.java
+++ app/src/main/java/hexlet/code/Greet.java
@@ -2,10 +2,11 @@ package hexlet.code;
 
 import java.util.Scanner;
 
-public class Cli {
-    public static void askUserName() {
+public class Greet {
+    public static void greetUser() {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Welcome to the Brian Game!");
         System.out.print("May I have your name? ");
         String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
