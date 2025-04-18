c2132a4f3e7070d81a04a9d35b77432987d8f6d8
Grouler <vad.abdurahmanov@gmail.com>
2025-04-19 00:48:37 +0300

    add Calc game

---
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -0,0 +1,54 @@
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+
+public class Calc {
+    public static void startGame() {
+        Engine.greetUser();
+        System.out.println("What is the result of the expression?");
+        String[] questions = questGenerator();
+        String[] answers = answerGenerator(questions);
+        Engine.runGame(questions, answers);
+    }
+
+    public static String[] questGenerator() {
+        Random random = new Random();
+        String[] questions = new String[3];
+        String[] operations = {"+", "-", "*"};
+
+        for (var i = 0; i < 3; i++) {
+            int a = random.nextInt(20) + 1;
+            int b = random.nextInt(20) + 1;
+            int randomOperation = random.nextInt(3);
+            questions[i] = a + " " + operations[randomOperation] + " " + b;
+        }
+        return questions;
+    }
+
+    public static String[] answerGenerator(String[] questions) {
+        String[] answers = new String[3];
+        for (var i = 0; i < 3; i++) {
+            String[] parts = questions[i].split(" ");
+            int a = Integer.parseInt(parts[0]);
+            int b = Integer.parseInt(parts[2]);
+            String operator = parts[1];
+            switch (operator) {
+                case "+":
+                    answers[i] = String.valueOf(a + b);
+                    break;
+                case "-":
+                    answers[i] = String.valueOf(a - b);
+                    break;
+                case "*":
+                    answers[i] = String.valueOf(a * b);
+                    break;
+                default:
+                    answers[i] = String.valueOf(0);
+                    break;
+            }
+        }
+        return answers;
+    }
+}
